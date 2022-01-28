40083d91fe302aaa6dff7ba17205e3b8c5524247
Akash B G <akashbpura@gmail.com>
2022-01-28 21:55:16 +0530

    Updated the code

--- HelloWorld.js
+++ HelloWorld.js
@@ -4,8 +4,7 @@ const num1 = 5;
 const num2 = 3;
 
 // add two numbers
-const sum = 
-num1 + num2;
+const sum = num1 + num2;
 
 // display the sum
 console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);
