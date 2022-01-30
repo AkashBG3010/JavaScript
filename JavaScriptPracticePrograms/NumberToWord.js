3cc3af1f4ff63a5afe323acaa50bb82150b3f64d
Akash B G <akashbpura@gmail.com>
2022-01-31 00:23:29 +0530

    Added PracticePrograms

---
+++ JavaScriptPracticePrograms/NumberToWord.js
@@ -0,0 +1,35 @@
+num = prompt('Enter a number between 0-9 to be converted to word: ')
+
+if (num == 0){
+console.log('Entered number is "ZERO"')
+}
+else if (num == 1){
+    console.log('Entered number is "ONE"')
+}
+else if (num == 2){
+    console.log('Entered number is "TWO"')
+}
+else if (num == 3){
+    console.log('Entered number is "THREE"')
+}
+else if (num == 4){
+    console.log('Entered number is "FOUR"')
+}
+else if (num == 5){
+    console.log('Entered number is "FIVE"')
+}
+else if (num == 6){
+    console.log('Entered number is "SIX"')
+}
+else if (num == 7){
+    console.log('Entered number is "SEVEN"')
+}
+else if (num == 8){
+    console.log('Entered number is "EIGHT"')
+}
+else if (num == 9){
+    console.log('Entered number is "NINE"')
+}
+else {
+    console.log('Invalid!! Please enter a number between 0-9')
+}
