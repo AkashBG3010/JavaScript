3d8319e7b15a1af6e38f05f9e1576b07f2ee7488
Akash B G <akashbpura@gmail.com>
2022-02-04 12:40:05 +0530

    Added Practice IfElse program

---
+++ JavaScriptPracticePrograms/ifStatements.js
@@ -0,0 +1,9 @@
+let age = prompt("Enter the age: ");
+console.log ("Entered age is: "+age);
+
+    if ((age >= 14) && (age < 19)) {
+        status = "Eligible";
+    }
+    else {
+        status = "Not eligible";
+}
